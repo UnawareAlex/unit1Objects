c38a7706fa46e4cc9e091e2d0e5c44e6e1052e52
UnawareAlex <agarnold@NN123-HA-05.HS.sd203.org>
2015-08-21 14:13:11 -0500

    I MADE A RED BACKGROUAGNJKLSBHDGKJSERKUGDF

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -5,18 +5,26 @@ public class TurtleWorld
     public static void main(String args[])
     {
         World turtleWorld = new World();
-        Turtle turtle = new Turtle(turtleWorld);
+        Turtle turtle = new Turtle(0, 0, turtleWorld);
         turtle.setPenColor(Color.RED);
         turtle.penDown();   
         Turtle turtle1 = new Turtle(turtleWorld);
-        turtle1.setPenColor(Color.GREEN);
-        turtle1.penDown();
-        turtle1.forward(20);
-        while (1 > 0) {
-     turtle.forward(5);
-        turtle.turn(5);
-     turtle1.forward(5);
-        turtle1.turn(5);
+        //turtle1.setPenColor(Color.GREEN);
+        //turtle1.penDown();
+        //turtle1.forward(30);
+        //int repeat = 0;
+        turtle.turn(180);
+        
+        while (1>0) {
+     
+     turtle.forward(500);
+     turtle.turn(270);
+     turtle.forward(1);
+     turtle.turn(270);
+     turtle.forward(500);
+     turtle.turn(90);
+     turtle.forward(1);
+     turtle.turn(90);
 }
         
     }
