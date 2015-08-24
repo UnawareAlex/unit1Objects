34dec676cf5b1490b6cbea2007d1972046d1ca7d
UnawareAlex <agarnold@NN123-HA-05.HS.sd203.org>
2015-08-24 14:12:59 -0500

    Jerva Stuff

---
+++ Chapter 1 Practice/HelloPrinter.java
@@ -0,0 +1,10 @@
+public class HelloPrinter
+{
+    public static void main(String[] args)
+    {
+        //display a greeting in the console window
+        while (1 > 0) {
+        System.out.println("Hello World!");
+    }
+}
+}
