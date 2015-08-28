094ced64e3a6eebe526f3d15cc6a076ea05e98b6
UnawareAlex <agarnold@NN123-HA-05.HS.sd203.org>
2015-08-28 14:11:23 -0500

    Objects and VARIBALES WATDHEK IS THIS

---
+++ Chapter 2 Practice/String2.java
@@ -0,0 +1,16 @@
+public class String2
+{
+    public static void main(String[] args)
+    {
+        String animal1 = "quick brown fox ";
+        String animal2 = "lazy dog ";
+        String article = "the ";
+        String action = "jumps over ";
+        
+        String message = (article + animal1 + action + article + animal2);
+        String message1 = article.concat(animal1.concat(action.concat(article.concat(animal2))));
+        
+        System.out.println(message);
+        System.out.println(message1);
+    }
+}
